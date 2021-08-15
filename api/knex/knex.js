e3ccd05eed34551df3c08affdbaaf9a374a58970
Mayes Elbaba <mayes.el.baba@student.ehb.be>
2021-08-15 19:36:28 +0200

    maken van database in dockerfile

--- api/knex/knex.js
+++ api/knex/knex.js
@@ -1,3 +1,39 @@
 const knex = require('knex');
 const knexfile = require('./knexfile');
+const db = knex(knexfile.config);
 module.exports = knex(knexfile.development);
+
+
+async function initialiseTables() {
+    await db.schema.hasTable('categories').then(async (exists) => {
+      if (!exists) {
+        await db.schema
+          .createTable('categories', (table) => {
+            table.uuid('uuid_category').primary();
+            table.string('name', 100).notNullable();
+          })
+          .then(async () => {
+            console.log('created table categories');
+          });
+  
+      }
+    });
+
+    await db.schema.hasTable('logs').then(async (exists) => {
+        if (!exists) {
+          await db.schema
+            .createTable('logs', (table) => {
+                table.uuid('uuid_log').primary();
+                table.text('text');
+                table.uuid('uuid_category_fk').references('uuid_category').inTable('categories').onDelete('CASCADE');
+            })
+            .then(async () => {
+              console.log('created table logs');
+            });
+    
+        }
+      });
+  }
+  
+  initialiseTables()
+  module.exports = db
--- api/knex/knexfile.js
+++ api/knex/knexfile.js
@@ -1,31 +1,11 @@
-// Update with your config settings.
-
 module.exports = {
-
-    development: {
+    config: {
         client: 'postgresql',
         connection: {
             database: 'dev_5_werkstuk',
             user: 'postgres',
             password: 'mayes',
             host:'db'
-
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
-    },
-
-    production: {
-        client: 'dev_5_werkstuk',
-        connection: {
-            database: 'dev_5',
-            user: 'postgres',
-            password: 'mayes'
         },
         pool: {
             min: 2,
@@ -35,5 +15,4 @@ module.exports = {
             tableName: 'knex_migrations'
         }
     }
-
-};
+  };
